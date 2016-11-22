d3a1e11e26e1e58a8726fd833db027b06aca5a48
francesca2 <francesca.giordani88@gmail.com>
2016-11-22 16:15:04 +0100

    messo a posto i servizi

--- AziendaJSF/src/main/java/it/alfasoft/francesca/service/Servizi.java
+++ AziendaJSF/src/main/java/it/alfasoft/francesca/service/Servizi.java
@@ -132,68 +132,6 @@ public class Servizi {
 		}
 	}
 
-	//metodo per creare una rubrica
-	public boolean registraRubrica(String username)
-	{
-		Rubrica r= new Rubrica(username);
-		boolean b= rdao.creaRubrica(r);
-		return b;
-	}
-	
-	//metodo per aggiungere una voce in rubrica
-	public boolean registraVoce(Rubrica r,Voce v) {
-		boolean result= false;
-		v.setRubrica(r);
-		r.addVoce(v);
-		boolean b=vdao.aggiungiVoce(v);
-		rdao.aggiornaRubrica(r);
-
-		if(b==true)
-		{
-			result =true;
-		}
-
-		return result;
-
-	}
-
-	//Metodo per trovare la rubrica di un utente a seconda del suo username
-	public Rubrica trovaRubrica(String username) {
-
-		Rubrica r= rdao.trovaRubricaConNome(username);
-
-		return r;
-	}
-	//metodo per trovare una voce
-	public Voce getVoceById(long id){
-		return vdao.trovaVoceConId(id);
-	}
-
-	//metodo per prendere tutte le voci di rubrica
-	public List<Voce> getVoci(Rubrica r) {
-		List<Voce> lista = vdao.getVociRubrica(r);
-
-		return lista;
-	}
-
-
-	//metodo per eliminare una voce
-	public boolean eliminaVoce(Rubrica r, long id)
-	{
-		Voce v=vdao.trovaVoceConId(id);
-		boolean result=vdao.eliminaVoce(v);
-		return result;
-	}
-	//metodo per aggiornare la rubrica
-	public boolean saveRubrica(Rubrica r){
-		return rdao.aggiornaRubrica(r);
-	}
-	
-	//metodo per aggiornare una voce
-	public boolean saveVoce(Voce v){
-		return vdao.aggiornaVoce(v);
-	}
-
 	//metodo per creare una busta paga
 	public boolean salvaBustaPaga(BustaPaga b) {
 		return bdao.aggiungiBustaPaga(b);
