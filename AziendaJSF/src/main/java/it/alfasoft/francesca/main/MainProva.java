bae11a8dff1f00acdce58676c8911dfb9beed031
francesca2 <francesca.giordani88@gmail.com>
2016-11-22 17:28:14 +0100

    jasper

--- AziendaJSF/src/main/java/it/alfasoft/francesca/main/MainProva.java
+++ AziendaJSF/src/main/java/it/alfasoft/francesca/main/MainProva.java
@@ -10,14 +10,16 @@ public class MainProva {
 	public static void main(String[] args) {
 		ServiziBusta sb=new ServiziBusta();
 		
-		Date data=new Date();
-		BustaPagaWS b1= new BustaPagaWS(data, 1200, "cod102");
-		BustaPagaWS b2= new BustaPagaWS(data, 1250, "cod103");
-		BustaPagaWS b3= new BustaPagaWS(data, 1800, "cod104");
+//		Date data=new Date();
+//		BustaPagaWS b1= new BustaPagaWS(data, 1200, "cod102");
+//		BustaPagaWS b2= new BustaPagaWS(data, 1250, "cod103");
+//		BustaPagaWS b3= new BustaPagaWS(data, 1800, "cod104");
+//		
+//		sb.addBustaPaga(b1);
+//		sb.addBustaPaga(b2);
+//		sb.addBustaPaga(b3);
 		
-		sb.addBustaPaga(b1);
-		sb.addBustaPaga(b2);
-		sb.addBustaPaga(b3);
+		
 
 	}
 
